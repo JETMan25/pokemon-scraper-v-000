a486ef112ee9f9432a4bb8045bf56536a57423d2
jontenthoff <jontenthoff@hotmail.com>
2019-05-14 14:51:54 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
   @@all = []
   
-  def initialize(id, name, type, database_connection)
+  def self.initialize(id, name, type, database_connection)
     @id = id
     @name = name
     @type = type
